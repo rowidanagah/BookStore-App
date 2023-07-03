c3f72413baf9418035cc349cbb908df09b037a4e
Rowida Nagah Mohamed <52299389+rowidanagah@users.noreply.github.com>
2023-07-03 15:12:55 +0300

    build backend side locally

---
+++ backend/books/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
+from .BookSerializers import *
+from .models import *
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+# Create your views here.
+
+class BookList(ListCreateAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+
+class BookDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    lookup_field = 'pk'
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        filter_kwargs = {self.lookup_field: self.kwargs[self.lookup_field]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    # def get_object(self):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     filter_kwargs = {self.lookup_field: uuid.UUID(self.kwargs[self.lookup_field])}
+    #     obj = get_object_or_404(queryset, **filter_kwargs)
+    #     self.check_object_permissions(self.request, obj)
+    #     return obj
